575207627b30623030d33fd839f12a3200005155
Luyen <luyenlaluot@gmail.com>
2025-10-05 22:31:10 +0700

    create some package

---
+++ assignment/src/main/java/model/User.java
@@ -0,0 +1,72 @@
+package model;
+
+public class User extends BaseModel implements java.io.Serializable {
+    private String username;
+    private String password;
+    private String fullname;
+    private String email;
+    private String phone;
+    private String address;
+    private int roleId;
+
+    public User() {
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+}
